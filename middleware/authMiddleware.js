e774257736dff17c717dad61439cf41eff5b7760
sulthann1 <sulthanaawaliya155@gmail.com>
2026-07-27 10:45:51 +0700

    menambahkan middleware otentikasi untuk verifikasi token JWT

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+    try {
+        const token = req.headers.authorization;
+
+        if (!authHeader) {
+            return res.status(401).json({ 
+            message: 'Authorization header missing' 
+        });
+
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({
+            message: 'Token missing'
+        });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded;
+    next();
+    } catch (error) {
+        return res.status(401).json({
+            message: 'Invalid token'
+        });
+    }   
+};
